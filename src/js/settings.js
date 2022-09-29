8c9e27a0714fe7a216db75ce834ee0cfb62202b8
Marek Skolik <marek250296@interia.pl>
2022-09-29 11:58:15 +0200

    commit

--- src/js/settings.js
+++ src/js/settings.js
@@ -3,7 +3,10 @@ export const select = {
 };
 
 export const settings = {
-
+    db: {
+        url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+        products: 'products',
+    }
 };
 
 export const classNames = {
