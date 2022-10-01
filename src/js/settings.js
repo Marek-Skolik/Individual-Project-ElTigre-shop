1a65916e0299a54efc92f802536db16e0d031907
Marek Skolik <marek250296@interia.pl>
2022-10-01 15:55:00 +0200

    commit

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,14 +1,27 @@
 export const select = {
-
+  templateOf: {
+    menuProduct: '#template-menu-product',
+  },
+  containerOf: {
+    menu: '#product-list',
+    pages: '#pages',
+  },
+  nav: {
+    links: '.navigation a',
+  },
 };
 
 export const settings = {
-    db: {
-        url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
-        products: 'products',
-    }
+  db: {
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    products: 'products',
+  }
 };
 
 export const classNames = {
 
+};
+
+export const templates = {
+  menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
 };
