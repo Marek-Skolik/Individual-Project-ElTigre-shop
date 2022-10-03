f1630a052e9d94701f6b27e1a088ec96266dd2f7
Marek Skolik <marek250296@interia.pl>
2022-10-03 16:56:59 +0200

    commit

--- src/js/components/Products.js
+++ src/js/components/Products.js
@@ -1,5 +1,19 @@
+import {templates} from '../settings.js';
 
 class Product {
+    constructor (element){
+        const thisProduct = this;
+
+        thisProduct.render(element);
+    }
+
+    render(element){
+        const thisProduct = this;
+        const generatedHTML = templates.productList();
+        thisProduct.dom = {};
+        thisProduct.dom.wrapper = element;
+        thisProduct.dom.wrapper.innerHTML = generatedHTML;
+    }
 }
 console.log(Product);
 
